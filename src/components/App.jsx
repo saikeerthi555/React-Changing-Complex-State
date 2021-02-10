b28f7cb8c54ce8ab04061d5810460af205bfd706
saikeerthi555 <43855677+saikeerthi555@users.noreply.github.com>
2021-02-10 20:06:47 +0530

    ComplexState

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  //destructuring useState
+  const [fname, setFName] = useState("");
+  const [lname, setLName] = useState("");
+
+  function updateFName(event) {
+    const firstName = event.target.value;
+    setFName(firstName);
+  }
+  function updateLName(event) {
+    const lastName = event.target.value;
+    setLName(lastName);
+  }
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>
+        Hello {fname}
+        {lname}
+      </h1>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
-        <button>Submit</button>
+        <input
+          name="fname"
+          onChange={updateFName}
+          placeholder="First Name"
+          value={fname}
+        />
+        <input
+          name="lname"
+          onChange={updateLName}
+          placeholder="Last Name"
+          value={lname}
+        />
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
